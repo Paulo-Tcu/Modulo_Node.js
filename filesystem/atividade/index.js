40a5368adaaef29c7e8822cae0c071ca1dc3f4e7
Paulo Cesar Querino da Cruz de Araujo <pcaraujo.tcu@gmail.com>
2022-03-06 23:09:15 -0300

    commit

---
+++ filesystem/atividade/index.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+const path = require('path');
+
+const filePatch = path.join('file.txt');
+fs.promises.readFile(filePatch, 'utf-8').then((resultado) => console.log(`Leitura do arquivo com Promisse:\n${resultado}`));
