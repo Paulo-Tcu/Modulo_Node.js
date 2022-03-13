7f2fe33bea926eab6806e50cce3a1841e7886bf0
Paulo Cesar Querino da Cruz de Araujo <pcaraujo.tcu@gmail.com>
2022-03-13 18:49:34 -0300

    commit

---
+++ 08-03/index.js
@@ -0,0 +1,35 @@
+require('dotenv').config({ override: true });
+const { DateTime } = require('luxon');
+
+//Criar um arquivo .env e adicionar uma variável NOME
+console.log(process.env.nome);
+
+console.log("--------------------LUXON -----------------------------------------\n");
+
+// Criar e imprimir o dia de ontem
+const today = DateTime.now();
+const yesterday = today.minus({ days: 1 });
+
+console.log(`Yesterday is: ${yesterday.toString()}`);
+
+// Criar o dia 5 do mês de julho desse ano às 15h usando o método fromObject
+const reallySpecificDate = DateTime.fromObject({ day: 5, month: 7, hour: 15 });
+console.log({ reallySpecificDate: reallySpecificDate.toLocaleString(DateTime.DATETIME_MED) });
+
+// Imprimir, em alemão, o dia da semana da data 15 dias antes da véspera de natal de 1980
+const fifteenDaysBefore1980ChristmasEve = (
+  DateTime
+    .fromObject({
+      day: 24,
+      year: 1980,
+    })
+    .setLocale('de')
+    .minus({
+      days: 15,
+    })
+    .toLocaleString({
+      weekday: 'long',
+    })
+);
+
+console.log({ fifteenDaysBefore1980ChristmasEve: fifteenDaysBefore1980ChristmasEve.toString() });
