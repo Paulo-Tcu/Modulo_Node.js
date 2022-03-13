f7a8813f193730dc413addf8c6eb77ac8756719c
Paulo Cesar Querino da Cruz de Araujo <pcaraujo.tcu@gmail.com>
2022-03-13 18:34:02 -0300

    commit

---
+++ network/index_api.js
@@ -0,0 +1,34 @@
+const fsPromise = require('fs').promises;
+const path = require('path');
+const https = require('https');
+
+function fetchPokemon(id = 1) {
+    const BASE_URL = 'https://pokeapi.co/api/v2/pokemon/';
+    const pokeURL = `${BASE_URL}${id}`;
+  
+    return new Promise((resolve, reject) => {
+      const req = https.request(pokeURL, res => {
+        const data = [];
+  
+        res.on('data', chunk => {
+          data.push(chunk);
+        });
+  
+        res.on('end', () => {
+          const pokemonRaw = Buffer.concat(data).toString();
+          const pokemonJSON = JSON.parse(pokemonRaw);
+          resolve(pokemonJSON.name);
+        });
+      })
+  
+      req.on('error', error => {
+        reject(error);
+      });
+  
+      req.end();
+    });
+  }
+
+  module.exports = {
+    fetchPokemon,
+  };
